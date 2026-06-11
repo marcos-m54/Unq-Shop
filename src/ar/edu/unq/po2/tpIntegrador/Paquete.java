af07b2904d612081553c126be40e0edc8a98ca03
maria-azurduy <mmaria.azurduy@gmail.com>
2026-06-11 00:18:19 -0300

    Codigo 2.1 (#1)
    * avance 2.1 general
    
    * codigo 2.1 basico

---
+++ src/ar/edu/unq/po2/tpIntegrador/Paquete.java
@@ -0,0 +1,76 @@
+package ar.edu.unq.po2.tpIntegrador;
+
+import java.util.ArrayList;
+
+public class Paquete implements IItem {
+	
+	public String nombre;
+	public String descripcion;
+	public int descuento;
+	public ArrayList<Producto> productos = new ArrayList<Producto>();
+	
+	public Paquete(String nombre, String descripcion, int descuento, ArrayList<Producto> productos) {
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.descuento = descuento;
+		this.productos = productos;
+	}
+
+	@Override
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	public int getDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(int descuento) {
+		this.descuento = descuento;
+	}
+	
+	//VER
+	@Override
+	public Double getPrecioBaseCalculado() {
+		return this.productos.stream()
+									 .mapToDouble(p -> p.getPrecioBaseCalculado())
+									 .sum();
+	}
+	
+	//VER
+	public Double calculoDePeso() {
+		return this.productos.stream()
+									 .mapToDouble(p -> p.getPeso())
+									 .sum();
+	}
+	
+	
+	public Double precioFinal() {
+		return getPrecioBaseCalculado() - this.getDescuento(); //revisar
+	}
+	
+	public void agregarProducto(Producto producto) {
+		this.productos.add(producto);
+	}
+	
+	public void quitarProducto(Producto producto) {
+		this.productos.remove(producto);
+	}
+	
+	
+	
+
+}
---
+++ src/ar/edu/unq/po2/tpIntegrador/Producto.java
@@ -0,0 +1,103 @@
+package ar.edu.unq.po2.tpIntegrador;
+
+import java.util.ArrayList;
+
+public class Producto implements IItem {
+	
+	private String SKU;
+	private String nombre;
+	private String marca;
+	private Double precio;
+	private int descuento;
+	private int stock;
+	private Double peso;
+	private String descripcion;
+	private ArrayList<Atributo> atributos = new ArrayList<Atributo>();
+	private Categoria categoria;
+	
+	public Producto(String sKU, String nombre, String marca, Double precio, int descuento, int stock, Double peso,
+			String descripcion, ArrayList<Atributo> atributo) {
+		
+		this.SKU = sKU;
+		this.nombre = nombre;
+		this.marca = marca;
+		this.precio = precio;
+		this.descuento = descuento;
+		this.stock = stock;
+		this.peso = peso;
+		this.descripcion = descripcion;
+		this.atributos = atributo;
+		
+	}
+	
+	public String getSKU() {
+		return SKU;
+	}
+
+	public void setSKU(String sKU) {
+		SKU = sKU;
+	}
+	
+	@Override
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public void setNombre(String nombreP) {
+		nombre = nombreP;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(int descuento) {
+		this.descuento = descuento;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+
+	@Override
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	
+	
+	public void agregarAtributo(String nombre, String descripcion) {
+		this.atributos.add(new Atributo(nombre, descripcion));
+	}
+	
+	@Override
+	public Double getPrecioBaseCalculado() {
+		return precio;
+	}
+
+}
