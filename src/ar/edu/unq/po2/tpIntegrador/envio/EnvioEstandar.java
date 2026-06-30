5972fa89aa766636ae830ab8c9fc342d6daf6ec2
yamilaclementi <yamilaclementi@gmail.com>
2026-06-30 20:33:03 -0300

    Test de Envio + Constructores Setter y Getter

--- src/ar/edu/unq/po2/tpIntegrador/envio/EnvioEstandar.java
+++ src/ar/edu/unq/po2/tpIntegrador/envio/EnvioEstandar.java
@@ -7,16 +7,28 @@ import java.util.concurrent.ThreadLocalRandom;
 public class EnvioEstandar implements IFormaDeEnvio {
 	
 	private ICorreoArgentina correoArgentina;
-	
+	// Nota Yami: Agregue constructor, setter y getter (uml)
+	public EnvioEstandar(ICorreoArgentina correoArgentina) {
+		super();
+	    this.correoArgentina = correoArgentina;
+	}
 
+	public ICorreoArgentina getCorreoArgentina() {
+	    return correoArgentina;
+	}
+
+	public void setCorreoArgentina(ICorreoArgentina correoArgentina) {
+	    this.correoArgentina = correoArgentina;
+	}
+	
 	@Override
 	public Double calcularValorDelEnvio(Pedido pedido) {
-		return correoArgentina.estimarEnvio(pedido.calcularValorDeEnvio(pedido), pedido.getUsuario().getDireccion());
+		return correoArgentina.estimarEnvio(pedido.pesoTotalDePedido(),
+				pedido.getUsuario().getDireccion());
 	}
 
 	@Override
 	public int estimacionDiasDeEnvio(Pedido pedido) {
 		return ThreadLocalRandom.current().nextInt(5, 8);
 	}
-
 }
